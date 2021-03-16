88df905ff8778676b854e7bdf2f62cb6c2117d96
reda1243 <redachikhoui12@gmail.com>
2021-03-16 17:18:52 +0200

    this is advenced calclutor

--- re.py
+++ re.py
@@ -1,20 +1,32 @@
-Num1 = float(input("enter number one "))
-operetor = input("enter operetor ")
-Num2 = float(input('enter number two '))
-def number(Num1,Num2,operetor):
-    if operetor == "+":
-        print(Num1 + Num2)
-        
-    elif operetor == "-":
-        print(Num1 - Num2)
-    elif operetor == "*":
-        print(Num1 * Num2)
-    elif operetor == "/":
-        print(Num1 / Num2)
-    elif operetor == '%':
-        print(Num1 % Num2)
-    elif operetor is not "+"and"/"and"+"and"*"and"%":
-        print('opertor not true use + or - or * or / or %')
-number(Num1,Num2,operetor)
-        
-        
+
+Num1 = float(input(' : '))
+Opertor = input('Enter opertor')
+Num2 = float(input(' : '))
+
+#this fontion for Addition
+if Opertor is '+':
+    def Add(Num1,Num2,opertor):
+        if opertor == "+":
+            return Num1 + Num2
+    print(Add(Num1,Num2,Opertor))
+
+#this fontion for Subtraction
+elif Opertor is "-":
+    def Sub(Num1,Num2,opertor):
+        if opertor == "-":
+            return Num1 + Num2
+    print(Sub(Num1,Num2,Opertor))
+
+#this fonction for Multiplication
+elif Opertor is "*":
+    def Mult(Num1,Num2,opertor):
+        if opertor == "*":
+            return Num1 + Num2
+    print(Mult(Num1,Num2,Opertor))
+
+#this fonction for Division
+elif Opertor is "/":
+    def Div(Num1,Num2,opertor):
+        if opertor == "/":
+            return Num1 + Num2
+    print(Div(Num1,Num2,Opertor))
